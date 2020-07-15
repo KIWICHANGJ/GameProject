beee58a66b38fb881c0afab1a148d9f8694e7713
Chung-RD <ken950333@gmail.com>
2020-07-15 19:46:25 +0800

    test upload

---
+++ fju/Assets/script/move.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    protected Joystick joystick;
+    protected Joybutton joybutton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        joystick = FindObjectOfType<Joystick>();
+        joybutton = FindObjectOfType<Joybutton>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var rigidbody = GetComponent<Rigidbody>();
+
+        rigidbody.velocity = new Vector3(joystick.Horizontal * 5f,
+                                         rigidbody.velocity.y,
+                                         joystick.Vertical * 5f);
+
+
+
+    }
+}
