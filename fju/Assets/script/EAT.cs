8c5eb7fad7f69865f85b0f83ce4eba5c94a230c4
Chung-RD <ken950333@gmail.com>
2020-08-07 13:58:50 +0800

    test

---
+++ fju/Assets/script/EAT.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class EAT : MonoBehaviour {
+
+    public GameObject player;
+    public Vector3 V3_random;
+    // Use this for initialization
+    void Start () {
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        V3_random = new Vector3(Random.Range(4.0f, -4.0f), 0.5f, Random.Range(4.0f, -4.0f));
+	}
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.transform.position = new Vector3((float)0, (float)0, (float)0);
+            gameObject.transform.position = V3_random;
+            Rigidbody rb = other.gameObject.GetComponent<Rigidbody>();
+            rb.velocity = Vector3.zero;
+            rb.angularVelocity = Vector3.zero;
+        }
+    }
+    
+}
+
---
+++ fju/Assets/script/hookmove.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class hookmove : MonoBehaviour
+{
+    protected Joybutton joybutton;
+    private Joystick joystick;
+    public float Hori;
+    public float Vert;
+    private Vector3 offset;
+    private bool isgo = false;
+    public GameObject player;
+    public float going;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+        joystick = FindObjectOfType<Joystick>();
+        joybutton = FindObjectOfType<Joybutton>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var rigidbody = GetComponent<Rigidbody>();
+        if (!isgo&&Input.GetKey(KeyCode.A))
+        {
+            Hori = joystick.Horizontal;
+            Vert = joystick.Vertical;
+            going = 600;
+            isgo = true;
+        }
+        if(going>0)
+        {
+            going -=1;
+            rigidbody.velocity = new Vector3(Hori * 10f,
+                                         rigidbody.velocity.y,
+                                         Vert* 10f);
+        }
+        else
+        {
+            isgo = false;
+            transform.position = player.transform.position + offset;
+            //爪子回到身上
+        }
+    }
+
+    
+}
+
